e87d61f466e97dc6eddc3aa6b0b76314e5a523d1
Howard Wang <wanghx@gmail.com>
2024-02-24 01:34:26 +0100

    wip

--- eml_extractor.py
+++ eml_extractor.py
@@ -61,8 +61,8 @@ def save_attachment(file: Path, payload: bytes) -> None:
         f.write(payload)
 
 
-def save_message(file: Path, message: str) -> None:
-    with file.open('w', encoding="utf8") as f:
+def save_message(file: Path, message: bytes) -> None:
+    with file.open('wb', encoding="utf8") as f:
         print(f'>> Saving cleaned email to "{file}"')
         f.write(message)
 
