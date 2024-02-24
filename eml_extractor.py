ced6e434d1f949a899df0698b45c56eb82dd9e03
Howard Wang <wanghx@gmail.com>
2024-02-24 15:15:22 +0100

    wip

--- eml_extractor.py
+++ eml_extractor.py
@@ -18,14 +18,14 @@ def extract_attachments(file: Path, destination: Path) -> None:
         with (file.open(mode='rb') as f):
             email_message = message_from_binary_file(f, policy=policy.default)
             save_policy = email_message.policy.clone(cte_type='8bit', utf8=True)
-            email_subject = email_message.get('Subject')
+            email_subject = email_message.get('Subject', failobj='')
             email_message.replace_header('Subject', email_subject)
             email_subject_file = "NoSubject" if len(email_subject) == 0 else email_subject[:max_len_subject]
-            email_from = email_message.get('From')
+            email_from = email_message.get('From', failobj='')
             email_message.replace_header('From', email_from)
-            email_to = email_message.get('To')
+            email_to = email_message.get('To', failobj='')
             email_message.replace_header('To', email_to)
-            email_cc = email_message.get('Cc')
+            email_cc = email_message.get('Cc', failobj='')
             email_message.replace_header('Cc', email_cc)
             from_addr = parseaddr(email_from)[1]
             email_date = email_message.get('Date')
